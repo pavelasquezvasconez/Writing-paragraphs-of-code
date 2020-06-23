11ea956d34af1fc0c080ddf8e06f3b9d6c472570
Alex <pavelasquezv@usp.br>
2020-06-23 10:20:06 -0300

    primer commit

---
+++ Writing paragraphs of code.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Conditional steps
+
+# In[8]:
+
+
+x = 15
+if x< 10:
+    print('smaller')
+if x> 20:
+    print('bigger')
+print('finis')
+
+
+# Repeated steps
+
+# In[11]:
+
+
+n=5
+while n>2:
+    print (n)
+    n=n-1
+print('Blastoff')
+
